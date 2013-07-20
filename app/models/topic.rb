d03b2d58cf9416be9d5d264e5e088fb93b54c28c
Rodrigo Dalcin <rodrigodalcin.dev@gmail.com>
2013-07-20 16:01:18 -0400

    Adds validation to topic model, removes edit link from topic index view.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   attr_accessible :description, :title, :url
 
   has_many :votes, dependent: :destroy
+
+  validates :description, :title, :url, presence: true
 end
