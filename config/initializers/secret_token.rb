6cf31ab00e87ba9d7742cd13eb32f722350bd088
Rodrigo Dalcin <rodrigodalcin.dev@gmail.com>
2013-07-20 12:02:09 -0400

    Initialize import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Suggestotron::Application.config.secret_token = '47e392b341cb5fad97ffa1aa228b55d8a51dfb78cf945c264c48d34a798663bc1529e248c9fa6c5064d19a6c96b1e6551224e549757a648e33c19b876d9d515c'
