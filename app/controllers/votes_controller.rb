2137eaa96ed3f28b8c86815bd6c9535d804a5f1c
Rodrigo Dalcin <rodrigodalcin.dev@gmail.com>
2013-07-20 15:37:23 -0400

    Implements voting feature.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,10 @@
 class VotesController < ApplicationController
+  def create
+    topic = Topic.find(params[:topic_id])
+    vote = topic.votes.new
+    vote.save!
+
+    redirect_to(topics_url)
+
+  end
 end
