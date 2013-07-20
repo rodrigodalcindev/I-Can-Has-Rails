a2f353bc64a99d476bbbf2d94588a60ad1844eb6
Rodrigo Dalcin <rodrigodalcin.dev@gmail.com>
2013-07-20 13:50:47 -0400

    Adds migration for topic URL.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130720174522) do
+ActiveRecord::Schema.define(:version => 20130720174956) do
 
   create_table "topics", :force => true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "url"
   end
 
 end
